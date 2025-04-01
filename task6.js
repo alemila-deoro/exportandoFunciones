8106976271c81fa155bcc764fb0dfe44810d0db5
Alexandra Pérez C. <alexaperezcardozo@gmail.com>
2025-03-31 23:51:04 -0500

    Complete la actividad

--- task6.js
+++ task6.js
@@ -1,3 +1,9 @@
-export function rubricExcellent() {
-
+export function rubricExcellent(puntuacion) {
+    let resultado = "";
+   if (puntuacion>8){
+    resultado = "excellent"
+   }
+   return resultado
 }
+
+console.log (rubricExcellent(11))
--- task7.js
+++ task7.js
@@ -1,3 +1,9 @@
-export function rubricPerfect() {
-
+export function rubricPerfect(puntuacion) {
+    let resultado = "";
+   if (puntuacion=11){
+    resultado = "perfect"
+   }
+   return resultado
 }
+
+console.log (rubricPerfect(11))
